93a7aeed4af00fb6105d7157bb7bae2b638c0515
wanganui <827052997@qq.com>
2017-06-02 16:08:02 +0800

    serviceimpl

--- src/main/java/com/hrg/serviceimpl/WorkerServiceImpl.java
+++ src/main/java/com/hrg/serviceimpl/WorkerServiceImpl.java
@@ -6,9 +6,12 @@ import com.hrg.javamapper.read.WorkerReadMapper;
 import com.hrg.model.Worker;
 import com.hrg.model.WorkerCriteria;
 import com.hrg.service.WorkerService;
+import com.hrg.util.PageUtil;
 import com.hrg.util.ValidUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by 82705 on 2017/6/1.
@@ -50,5 +53,38 @@ public class WorkerServiceImpl implements WorkerService {
         example.setAccount(account);
         Worker worker = workerReadMapper.selectByExampleForOne(example);
         return worker.getPassword();
+    }
+
+    /**
+     * 分页查询员工列表
+     *
+     * @param example  条件实体
+     * @param pageUtil 分页条件
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public PageUtil<Worker> selectByExample(WorkerCriteria example, PageUtil pageUtil) throws Exception {
+        if (pageUtil.getCurrentPage() !=null && pageUtil.getCurrentPage() <= 0)
+            throw new ValidatorException(ErrorCode.ILLEGALARGUMENT_EXCEPTION.getCode());
+
+        if (pageUtil.getPageSize() !=null && pageUtil.getPageSize() <= 0)
+            throw new ValidatorException(ErrorCode.ILLEGALARGUMENT_EXCEPTION.getCode());
+
+        pageUtil.setCurrentPage(pageUtil.getCurrentPage() == null ? 1 : pageUtil.getCurrentPage());
+
+        if ("".equals(example.getState()))
+            example.setState(null);
+
+        //设置分页参数
+        example.setPageSize((pageUtil.getPageSize() == null) ? 8 : pageUtil.getPageSize());
+        example.setLimitStart((pageUtil.getCurrentPage()  == null) ? 0 : (pageUtil.getCurrentPage()-1) * 8);
+        List<Worker> workerList = workerReadMapper.selectByExample(example);
+
+        int count = workerReadMapper.countByExample(example);
+        PageUtil<Worker> workerPageUtil = new PageUtil<Worker>(pageUtil.getPageSize(),count);
+        workerPageUtil.generate(pageUtil.getCurrentPage());
+        workerPageUtil.setPageResults(workerList);
+        return workerPageUtil;
     }
 }
