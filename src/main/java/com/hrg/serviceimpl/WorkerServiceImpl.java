401ddc9807db376ea2ed10cc737f80c22f2a8b6e
wanganui <827052997@qq.com>
2017-07-20 17:08:22 +0800

    主页修改

--- src/main/java/com/hrg/serviceimpl/WorkerServiceImpl.java
+++ src/main/java/com/hrg/serviceimpl/WorkerServiceImpl.java
@@ -477,7 +477,6 @@ public class WorkerServiceImpl implements WorkerService {
         param.put("first",first);
         param.put("currentTime",currentTime);
         param.put("departmentdataid",worker.getDepartmentdataid());
-
         map.put("workDataMissingLog",workdataReadMapper.selectMissingLog(param));
         return map;
     }
